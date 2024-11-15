95917783e9ad7afe669251bfb427c8c49f6703c1
raniahamdy300 <eng.raniahamdy300@gmail.com>
2024-11-15 03:48:11 +0200

    Add files via upload

---
+++ session4/lib/Todo.dart
@@ -0,0 +1,8 @@
+class Todo {
+  final String description;
+  final DateTime duedate;
+  final bool status;
+
+  Todo(
+      {required this.description, required this.duedate, required this.status});
+}
