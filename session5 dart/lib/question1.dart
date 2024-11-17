af98e152bb4feb962b091ec40656f19f4483b534
raniahamdy300 <eng.raniahamdy300@gmail.com>
2024-11-17 20:35:49 +0200

    Add files via upload

---
+++ session5 dart/lib/question1.dart
@@ -0,0 +1,7 @@
+// Exercise 1: Dart Basics
+// Write a Dart program to print "Hello, Dart!" to the console.
+// Goal: Learn how to use the main() function and print statements.
+
+void main() {
+  print('Hello, Dart!');
+}
